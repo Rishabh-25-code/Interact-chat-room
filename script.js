5da52055b9dbff9adb8239d828e80b6dff7c11b1
Rishabh Prakash <72189098+Rishabh-25-code@users.noreply.github.com>
2021-07-03 19:04:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+const socket = io('http://localhost:3000')
+
+const messageContainer = document.getElementById('message-container')
+const messageForm = document.getElementById('send-container')
+const messageInput = document.getElementById('message-input')
+
+const name = prompt('Enter your Name:');
+appendMessage('You joined');
+
+socket.emit('new-user', name)
+socket.on('chat-message', data => {
+    appendMessage(`${data.name}: ${data.message}`)
+})
+
+socket.on('user-connected', name => {
+    appendMessage(`${name} connected !!`)
+})
+
+socket.on('user-disconnected', name => {
+    appendMessage(`${name} disconnected !!`)
+})
+
+messageForm.on('submit', e => {
+    e.preventDefault()
+    const message = messageInput.value
+    appendMessage(`You: ${message}`)
+    socket.emit('send-chat-message', message)
+    messageInput.value = ''
+})
+
+function appendMessage(message) {
+    const messageElement = document.createElement('div');
+    // messageElement.style.backgroundColor = 'red';
+    messageElement.innerText = message;
+    messageContainer.append(messageElement);
+
+}
